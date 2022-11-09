8aa0bb2b13a01cc9b1266b9688a42b9449556e9e
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-11-09 14:34:21 +0530

    added code

--- module/vpc.tf
+++ module/vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc" {
     cidr_block = var.VPC_CIDR
     tags = {
         Name    = local.VPC_NAME
-        IAC     = local.IACTOOL
+        IACTOOL    = local.IAC
     }
   
 }
