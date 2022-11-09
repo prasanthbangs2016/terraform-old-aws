12b50fb7d32cc4bd28ceb8e8545b2981069bdbb0
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-11-10 01:00:23 +0530

    added code

--- module/vpc/routetable.tf
+++ module/vpc/routetable.tf
@@ -11,7 +11,29 @@ resource "aws_route_table" "route-table-private" {
     }
 }
 
+resource "aws_route_table" "route-table-public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = data.aws_vpc.management.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default-vpc-to-roboshop-vpc.id
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    #gateway id is nothing but igw id
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+   tags = {
+        Name        = "${local.VPC_NAME}-Private-Route-Table"
+        IACTOOL     = local.IAC
+    }
+}
+
 resource "aws_route" "management-vpc-peering-route" {
+  #route table id expects a string hence convered with locals and added here.
   route_table_id            = local.MANAGEMENT_ROUTE_TABLE
   destination_cidr_block    = var.VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.default-vpc-to-roboshop-vpc.id
--- module/vpc/subnet.tf
+++ module/vpc/subnet.tf
@@ -1,10 +1,24 @@
+resource "aws_subnet" "public-subnets" {
+  count     = 1
+  vpc_id    = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(var.VPC_CIDR, 8, count.index)
+  #when we enable this it wont create public ip and will have private ip
+  map_public_ip_on_launch = false
+
+     tags = {
+        Name    = "Roboshop-Dev-public-Subnet-${count.index+1}"
+        IACTOOL    = local.IAC
+    }
+}
 resource "aws_subnet" "private-subnets" {
   count     = length(data.aws_availability_zones.available.names)
   vpc_id    = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.VPC_CIDR, 8, count.index)
+  #when we enable this it wont create public ip and will have private ip
+  map_public_ip_on_launch = false
 
      tags = {
-        Name    = "Roboshop-Dev-Subnet-${count.index+1}"
+        Name    = "Roboshop-Dev-private-Subnet-${count.index+1}"
         IACTOOL    = local.IAC
     }
 }
@@ -15,4 +29,11 @@ resource "aws_route_table_association" "private-route-table-associate" {
   #taking 1 by 1 subnet attaching route table
   subnet_id      = element(aws_subnet.private-subnets.*.id, count.index)
   route_table_id = aws_route_table.route-table-private.id
+}
+
+resource "aws_route_table_association" "public-route-table-associate" {
+  count     = length(data.aws_availability_zones.available.names)
+  #taking 1 by 1 subnet attaching route table
+  subnet_id      = element(aws_subnet.public-subnets.*.id, count.index)
+  route_table_id = aws_route_table.route-table-public.id
 }
