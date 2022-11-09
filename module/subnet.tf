5051d3197cbdd2e9ff5d935a9ca0e911c08b46d5
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-11-09 15:17:27 +0530

    added code

--- module/subnet.tf
+++ module/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main" {
   count     = length(data.aws_availability_zones.available.id)
-  vpc_id    = aws_vpc.VPC.id
+  vpc_id    = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.VPC_CIDR, 8, count.index)
 
      tags = {
