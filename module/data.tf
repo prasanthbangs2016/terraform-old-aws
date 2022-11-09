5189221e3393767402b906dcd3fd9f4b99c82e75
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-11-09 15:25:26 +0530

    added code

--- module/data.tf
+++ module/data.tf
@@ -3,9 +3,11 @@ data "aws_availability_zones" "available" {
     state   = "available"
 }
 
+#to check
 resource "null_resource" "null" {
     provisioner "local-exec" {
-        command = "echo ${length(data.aws_availability_zones.available.id)}"
+        #command = "echo ${data.aws_availability_zones.available.id)}"
+        command = "echo ${data.aws_availability_zones.*.id}"
       
     }
   
